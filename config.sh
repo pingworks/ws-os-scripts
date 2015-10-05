ed57a49e8adcec813de737cff7355fd1f8338438
Christoph Lukas <lukas@pingworks.de>
2015-10-05 08:54:19 +0200

    update image versions, add image for testenv

--- config.sh
+++ config.sh
@@ -6,7 +6,9 @@ BASEDOMAIN="ws.pingworks.net"
 OS_AUTH_URL=http://$OS_CTRL:5000/v2.0
 COMPUTE_NODES="ctrl compute1 compute2"
 DOCKER_BASE_IMG="pingworks/docker-ws-baseimg:0.2"
-DOCKER_JKMASTER_IMG="pingworks/docker-ws-jkmaster:0.2"
-DOCKER_JKSLAVE_IMG="pingworks/docker-ws-jkslave:0.2"
+DOCKER_JKMASTER_IMG="pingworks/docker-ws-jkmaster:0.3"
+DOCKER_JKSLAVE_IMG="pingworks/docker-ws-jkslave:0.3"
+DOCKER_FRONTEND_IMG="pingworks/docker-ws-frontend:0.3"
+DOCKER_BACKEND_IMG="pingworks/docker-ws-backend:0.3"
 DEBUG=0
 unset USER
